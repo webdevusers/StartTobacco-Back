8b4b7f6e2879496148c2bbc0072ab3eca921a6a8
XGroup <138381865+webdevusers@users.noreply.github.com>
2023-10-28 15:49:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 const authRouter = require('./Auth/Router/AuthRouter')
 const productRouter = require('./Products/Router/Router')
 
+        const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
@@ -16,6 +17,10 @@ app.use("/items", productRouter)
 const start = async () => {
     try {
         await mongoose.connect('mongodb+srv://devusersvue:jXSFM1kfpDMF7RB7@starttobacco.qp2fq2p.mongodb.net/?retryWrites=true&w=majority');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
     } catch (e) {
         console.log(e)
     }
