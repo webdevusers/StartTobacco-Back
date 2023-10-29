6d620eccb722497ac04cc8ed9b5b8fb93eb68dc7
XGroup <138381865+webdevusers@users.noreply.github.com>
2023-10-29 22:10:30 +0100

    Update AuthRouter.js

--- Auth/Router/AuthRouter.js
+++ Auth/Router/AuthRouter.js
@@ -6,5 +6,6 @@ router.post('/registration', controller.registration)
 router.post('/authorization', controller.authorization )
 router.post('/changeRole', controller.changeRole)
 router.get('/get', controller.getUsers)
-
+router.post('/like', controller.likedProduct)
+router.post('/dislike', controller.dislikeProduct)
 module.exports = router;
