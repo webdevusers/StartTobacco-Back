aeffb9f98eb8af841d5d48ce216693027147b1c8
XGroup <138381865+webdevusers@users.noreply.github.com>
2023-11-10 01:30:38 +0100

    Update router.js

--- Orders/router/router.js
+++ Orders/router/router.js
@@ -5,5 +5,6 @@ const controller = require('../controller/controller')
 router.post('/create', controller.create)
 router.get('/get', controller.get)
 router.post('/delete', controller.delete)
+router.get('/getUnviewed', controller.getUnviewed)
 
 module.exports = router;
