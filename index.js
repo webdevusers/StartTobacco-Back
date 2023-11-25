3517ca54254b06efe03dfc6324c2bcb0ec401966
XGroup <138381865+webdevusers@users.noreply.github.com>
2023-11-25 21:09:17 +0100

    Finnaly back-end

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5175;
 const authRouter = require('./Auth/Router/AuthRouter')
 const productRouter = require('./Products/Router/Router')
 const orderRouter = require('./Orders/router/router')
+const reviewRouter = require('./Reviews/router/router')
+const msgRouter = require('./Messages/router/router')
 
 app.use(cors())
 app.use(express.json())
@@ -14,6 +16,9 @@ app.use(express.json())
 app.use("/user", authRouter)
 app.use("/items", productRouter)
 app.use("/order", orderRouter)
+app.use("/review", reviewRouter)
+app.use("/msg", msgRouter)
+
 const start = async () => {
     try {
         await mongoose.connect('mongodb+srv://devusersvue:jXSFM1kfpDMF7RB7@starttobacco.qp2fq2p.mongodb.net/?retryWrites=true&w=majority');
